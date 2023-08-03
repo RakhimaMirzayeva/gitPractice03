609171f14d16a4369b331d57271a9969d9377fca
rakhimamirzayeva <rakhimamirzayeva91@gmail.com>
2023-08-02 21:07:27 -0400

    new statement

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("this is a new code");
-        System.out.println("this is a new project" +
-                "");
+        System.out.println("this is a new project");
+        System.out.println("new statement");
     }
 }
